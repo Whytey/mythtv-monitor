702b2eefc3c47a68512b31dbb0ced8a596310456
David Whyte <david@thewhytehouse.org>
2013-09-08 21:21:33 +1000

    Minor updates to the event monitor

--- monitor/eventmonitor.py
+++ monitor/eventmonitor.py
@@ -21,20 +21,21 @@ class SystemEventMonitor(GObject.GObject, BEEventMonitor):
     
     def __init__(self):
         GObject.GObject.__init__(self)
-
+        
+        self.__script_name = 'mythtv-monitor'
+        
         BEEventMonitor.__init__(self, '192.168.0.100', False, True, None)
-        self.__logger = logging.getLogger("monitor.Blah")
+        self.__logger = logging.getLogger(__name__)
         self.__logger.info("Initialised")
 
 
     def _neweventconn(self):
-        return BEEventConnection(self.host, self.port, 'test-script', level = 3)
-
-    def eventMonitor(self, event=None):
-        if event is None:
-            return re.compile('.*SYSTEM_EVENT')
-        self.__logger.debug("Got a raw event: %s" % event)
-
+        return BEEventConnection(self.host, 
+                                 self.port, 
+                                 self.__script_name, 
+                                 level = 3)
+        
+    def __parse_event(self, event):
         payload = event.split(BACKEND_SEP)[1].split(" ")[1:]
         
         msg = {}
@@ -42,6 +43,16 @@ class SystemEventMonitor(GObject.GObject, BEEventMonitor):
         for i in range(1, len(payload[1:]), 2):
             msg[payload[i].lower()] = payload[i+1]
             
+        return msg
+
+
+    def eventMonitor(self, event=None):
+        if event is None:
+            return re.compile('.*SYSTEM_EVENT')
+        self.__logger.debug("Got a raw event: %s" % event)
+        
+        msg = self.__parse_event(event)
+
         self.__logger.debug("Got a payload of: %s" % msg)
         
         self.emit(self.SYSTEM_EVENT, msg)
